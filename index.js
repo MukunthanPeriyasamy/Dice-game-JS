663aba99d1aea4ccfa83020784520826a1cc9ace
Mukunthan Periyasamy <129720936+MukunthanPeriyasamy@users.noreply.github.com>
2024-08-02 20:34:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var number1 = Math.floor(Math.random()*6)+1;
+
+var image1 = "imges/image"+number1+".jpeg";
+
+document.querySelectorAll("img")[0].setAttribute("src",image1);
+
+
+var number2 = Math.floor(Math.random()*6)+1;
+
+var image2 = "imges/image"+number2+".jpeg";
+
+document.querySelectorAll("img")[1].setAttribute("src",image2);
